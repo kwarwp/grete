0b509abca34b1728a10841431b2e3b4cfd914224
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 13:47:52 -0300

    Created morgan/main.py at Timestamp: 2018-06-11 16:47:52

---
+++ morgan/main.py
@@ -0,0 +1,12 @@
+# grete.morgan.main.py
+#Principals room 1 = "https://imgur.com/a/NwXyGcM?s=wa"
+#"https://imgur.com/a/rXKCxUa?s=wa"
+#"https://imgur.com/a/zLJSBWR?s=wa"
+#"https://imgur.com/a/Aw1gkVs?s=wa"
+#objects
+#clips = "https://i.imgur.com/Z25C40V.jpg"
+#fechadura = "https://i.imgur.com/q1tJiqJ.jpg"
+#teclado = "https://i.imgur.com/EM4mZq2.jpg"
+#tomada telefonica ="https://i.imgur.com/4eX2x0y.jpg"
+#fiação eletrica = "https://i.imgur.com/IWX6ah1.jpg"
+# ? = "https://i.imgur.com/THLu9uv.jpg"
