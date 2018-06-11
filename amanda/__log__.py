723911ed016b61438ba475a53ce6adcd88ced606
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 11:03:28 -0300

    Created amanda/__log__.py at Timestamp: 2018-06-11 14:03:27

---
+++ amanda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 11 2018 11:03:26.29 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 38
+  x = 30, y = 500, w = 100, h = 120, drop="volcano",
+                                         ^
+SyntaxError: keyword argument repeated
+'''},
