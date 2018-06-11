9c4f6d17a6d402793e920564012c0829cfcd0d98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 14:34:20 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-06-11 17:34:20

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -17,3 +17,21 @@
     meio.vai = gogeo
 NameError: name 'meio' is not defined
 '''},
+{'date': 'Mon Jun 11 2018 14:34:18.396 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 54
+    trigonometria().norte.vai()
+  module <module> line 35
+    TRIG = _sala = Sala(CLASE1,CLASSE2,CLASSE3,CLASSE4, "trig")
+NameError: name 'CLASE1' is not defined
+'''},
