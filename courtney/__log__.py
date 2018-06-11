a55188625c444d4e7cc7883285b9ae7d083e06e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 15:55:33 -0300

    Saved courtney/__log__.py at Timestamp: 2018-06-11 18:55:33

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -139,3 +139,21 @@ NameError: name 'school_house_e' is not defined
     school_house = _sala = Sala(school_house_n,school_house_e,school_house_s,school_house_o, "trig")
 NameError: name 'school_house_e' is not defined
 '''},
+{'date': 'Mon Jun 11 2018 15:55:32.758 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    school_houser().norte.vai()
+  module <module> line 28
+    napkin_holder = Elemento(ball, tit = "vase", drag=True,
+NameError: name 'ball' is not defined
+'''},
