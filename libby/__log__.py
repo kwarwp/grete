de5bcd84caf27acd9728cf94ea67c1d2a805a4fb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 14:16:08 -0300

    Created libby/__log__.py at Timestamp: 2018-06-11 17:16:08

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 11 2018 14:15:58.307 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  CASAL = "https://images.vexels.com/media/users/3/129903/isolated/preview/c996f5193090b3a642ffc069bc81da0c-silhueta-do-casal-andando-12-by-vexels.png
+                                                                                                                                                      ^
+SyntaxError: EOL while scanning string literal
+'''},
