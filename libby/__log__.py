01c0988b56f68628a4ea29d03a714b3dc87d8e46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 14:16:48 -0300

    Saved libby/__log__.py at Timestamp: 2018-06-11 17:16:44

--- libby/__log__.py
+++ libby/__log__.py
@@ -6,3 +6,19 @@
                                                                                                                                                       ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Jun 11 2018 14:16:42.804 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    loversatthecamp()
+NameError: name 'loversatthecamp' is not defined
+'''},
