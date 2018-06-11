cae456390d6cf21f84c337614712813348ee9577
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 15:48:12 -0300

    Created courtney/__log__.py at Timestamp: 2018-06-11 18:48:12

---
+++ courtney/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Mon Jun 11 2018 15:48:10.27 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    trigonometria().norte.vai()
+  module <module> line 25
+    TRIG = _sala = Sala(trig_n,trig_e,trig_s,trig_o, "trig")
+NameError: name 'Sala' is not defined
+'''},
