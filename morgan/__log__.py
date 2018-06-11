75059a1738fc201205ff0427a8748b4f1b590f52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 14:09:14 -0300

    Created morgan/__log__.py at Timestamp: 2018-06-11 17:09:12

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 11 2018 14:09:10.95 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  Principals room 1 = "https://imgur.com/a/NwXyGcM?s=wa"
+              ^
+SyntaxError: invalid syntax
+'''},
