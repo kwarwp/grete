f69b91787a25b155b42d7d3b8311e26695ae72af
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 15:47:07 -0300

    Saved morgan/main.py at Timestamp: 2018-06-11 18:47:07

--- morgan/main.py
+++ morgan/main.py
@@ -5,10 +5,10 @@ Principals_room_l = "https://i.imgur.com/SV1Ko1N.jpg"
 Principals_room_s = "https://i.imgur.com/V85pfUS.jpg"
 # objects
 clips = "https://i.imgur.com/Z25C40V.jpg"
-fechadura = "https://i.imgur.com/q1tJiqJ.jpg"
-teclado = "https://i.imgur.com/EM4mZq2.jpg"
-tomada_telefonica ="https://i.imgur.com/4eX2x0y.jpg"
-fiacao_eletrica = "https://i.imgur.com/IWX6ah1.jpg"
+door_lock = "https://i.imgur.com/q1tJiqJ.jpg"
+keyboard = "https://i.imgur.com/EM4mZq2.jpg"
+power_plug ="https://i.imgur.com/4eX2x0y.jpg"
+pipe = "https://i.imgur.com/IWX6ah1.jpg"
 caixa_mae = "https://i.imgur.com/THLu9uv.jpg"
 from _spy.vitollino.main import STYLE, INVENTARIO, Sala
 STYLE["width"] = 800
@@ -25,17 +25,26 @@ def trigonometria():
     PRR = _sala = Sala(Principals_room_n,Principals_room_o,Principals_room_l,Principals_room_s, "principal")
     from naomi.main import Elemento
     _sala.sul.meio.vai = vai_geo
-    '''
-    vdgball = Elemento(ball, tit = "microscope", drag=True,
-        x = 610, y = 140, w = 80, h = 90, drop="glow ball",
-        cena=_sala.oeste, texto="please, help me, fix my name")
-    eglobe = Elemento(globe, tit = "volcano", drag=True,
-        x = 160, y = 210, w = 80, h = 100, drop="earth globe",
+    caixa = Elemento(caixa_mae, tit = "pipe", drag=True,
+        x = 480, y = 140, w = 180, h = 290, drop="glow ball",
+       cena=_sala.norte, texto="please, help me, fix my name")
+    clip  = Elemento(clips, tit = "power plug", drag=True,
+        x = 260, y = 410, w = 80, h = 100, drop="clips",
         cena=_sala.leste, texto="please, help me, fix my name")
-    volc = Elemento(volcano, tit = "glow ball", drop="volcano",
-        x = 30, y = 500, w = 100, h = 120,
+
+    pipes = Elemento(pipe, tit = "motherboard", drop="cano",
+        x = 500, y = 5, w = 100, h = 120,
+        cena=_sala.norte, texto="please, help me, fix my name")
+    power = Elemento(power_plug, tit = "pipe", drag=True,
+        x = 510, y = 360, w = 100, h = 90, drop="glow ball",
+       cena=_sala.leste, texto="please, help me, fix my name")
+    doorl = Elemento(door_lock, tit = "keyboard", drag=True,
+        x = 440, y = 380, w = 80, h = 100, 
+        cena=_sala.sul, texto="please, help me, fix my name")
+    keyboard_ = Elemento(keyboard, tit = "clip", drop="keyboard",
+        x = 300, y = 300, w = 100, h = 70,
         cena=_sala.leste, texto="please, help me, fix my name")
-    '''
+
     return _sala
 
 
