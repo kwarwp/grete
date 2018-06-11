343e6295c21e9615d5c4c9c8692c4f97e66fac97
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 12:52:47 -0300

    Saved naomi/__log__.py at Timestamp: 2018-06-11 15:52:45

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -60,3 +60,19 @@ IndentationError: expected an indented block
     pan = Elemento(PAN, tit="earth globe", drag=False, drop="sweep pan",
 NameError: name 'panstyle' is not defined
 '''},
+{'date': 'Mon Jun 11 2018 12:52:45.252 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 193
+    GEO.oeste.vai()
+AttributeError: 'NoneType' object has no attribute 'oeste'
+'''},
