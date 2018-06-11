1601944984cdbd0a70b5a98b4b858f95b3e5be87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-11 15:29:24 -0300

    Created sarah/__log__.py at Timestamp: 2018-06-11 18:29:23

---
+++ sarah/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Mon Jun 11 2018 15:29:21.517 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    trigonometria().norte.vai()
+  module <module> line 24
+    TRIG = _sala = Sala(QUADRA_n,QUADRA_l,QUADRA_o,QUADRA_s, "quadra")
+NameError: name 'QUADRA_l' is not defined
+'''},
