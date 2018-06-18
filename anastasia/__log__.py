bc9cc4deb245627c62a95ff075810275b664b4a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-18 15:35:20 -0300

    Created anastasia/__log__.py at Timestamp: 2018-06-18 18:35:20

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 18 2018 15:35:18.246 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  bars = "https://i.imgur.com/4g9dJdv.jpg”
+                                          ^
+SyntaxError: EOL while scanning string literal
+'''},
