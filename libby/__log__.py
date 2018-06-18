a29ba574a40a1e969705173471ba5b8d58d1b4f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-18 14:23:00 -0300

    Saved libby/__log__.py at Timestamp: 2018-06-18 17:23:00

--- libby/__log__.py
+++ libby/__log__.py
@@ -60,3 +60,21 @@ NameError: name 'ball' is not defined
 'error': '''
 Exception: <SyntaxError: Illegal return statement>
 '''},
+{'date': 'Mon Jun 18 2018 14:22:58.592 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 93
+    trigonometria().norte.vai()
+  module <module> line 78
+    _sala.sul.meio = Cena(hq, vai = vai_geo)
+NameError: name 'hq' is not defined
+'''},
