1d3612ce3dea72a35fba7ab68432ee6ad35cec5a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-18 15:03:22 -0300

    Created anastasia/main.py at Timestamp: 2018-06-18 18:03:22

---
+++ anastasia/main.py
@@ -0,0 +1,11 @@
+# grete.anastasia.main.py
+children = "https://i.imgur.com/SAjYpW5.jpg"
+bars = "https://i.imgur.com/4g9dJdv.jpg”
+grandstand = "https://i.imgur.com/6PYwS0s.jpg”
+reflector = "https://i.imgur.com/U41sbtV.jpg”
+light_post = "https://i.imgur.com/Jk68bvK.jpg”
+ball = "https://i.imgur.com/bMUHNdH.jpg”
+gimnasium_e = "https://i.imgur.com/RVIqCjp.jpg”
+gimnasium_l = "https://i.imgur.com/kdnYpkh.jpg”
+gimnasium_n = "https://i.imgur.com/befIyyM.jpg”
+gimnasium_s = "https://i.imgur.com/5SnGZmp.jpg”
