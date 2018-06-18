f86552c99d72e03f0d1ef6241dc436b8403ad1cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-18 14:16:04 -0300

    Saved stacy/__log__.py at Timestamp: 2018-06-18 17:16:03

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -90,3 +90,21 @@ NameError: name 'INVENTARIO' is not defined
     TRIG = _sala = Sala(CLASS23A,CLASS23B,CLASS23C,CLASS23D, "CLASS23")
 NameError: name 'Sala' is not defined
 '''},
+{'date': 'Mon Jun 18 2018 14:16:03.366 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 63
+    trigonometria().norte.vai()
+  module <module> line 49
+    vdgball = Elemento(ball, tit = "microscope", drag=True,
+NameError: name 'ball' is not defined
+'''},
