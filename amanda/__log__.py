c2819470aa9e9736b751beb4a8c64d69351d637b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-18 14:33:46 -0300

    Saved amanda/__log__.py at Timestamp: 2018-06-18 17:33:46

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -42,3 +42,21 @@ AttributeError: 'tuple' object has no attribute 'sul'
     _sala.sul.meio = Cena(hq, vai = vai_geo)
 NameError: name 'Cena' is not defined
 '''},
+{'date': 'Mon Jun 18 2018 14:33:44.738 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 65
+    trigonometria().norte.vai()
+  module <module> line 50
+    _sala.sul.meio = Cena(hq, vai = vai_geo)
+NameError: name 'hq' is not defined
+'''},
