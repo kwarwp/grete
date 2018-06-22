5070f821ceda4b19e90ea2e4ac714d14a08eb61e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-22 13:57:17 -0300

    Saved anastasia/__log__.py at Timestamp: 2018-06-22 16:57:17

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -34,3 +34,10 @@ SyntaxError: EOL while scanning string literal
                                           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Fri Jun 22 2018 13:57:13.939 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  bars = "https://i.imgur.com/4g9dJdv.jpgâ
+                                            ^
+SyntaxError: EOL while scanning string literal
+'''},
