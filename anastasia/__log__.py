693992ff2c6274ecd741401e29e90e5f2a79239c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-22 14:15:48 -0300

    Saved anastasia/__log__.py at Timestamp: 2018-06-22 17:15:47

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -144,3 +144,19 @@ NameError: name 'gimnasiumr' is not defined
     gimnasiums().norte.vai()
 NameError: name 'gimnasiums' is not defined
 '''},
+{'date': 'Fri Jun 22 2018 14:15:43.942 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    Gimnasium().norte.vai()
+NameError: name 'Gimnasium' is not defined
+'''},
