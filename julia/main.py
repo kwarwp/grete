d0f0d1550245bed6445427c08768f8989d55ecdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-22 15:38:09 -0300

    Saved julia/main.py at Timestamp: 2018-06-22 18:38:08

--- julia/main.py
+++ julia/main.py
@@ -1,11 +1,10 @@
 # grete.julia.main.py
 from _spy.vitollino.main import STYLE, INVENTARIO, Sala, Texto, Cena
 
-jogadordefutebol = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiOrfua7ufbAhWEnJAKHRaFBb0QjRx6BAgBEAU&url=https%3A%2F%2Fpt.pngtree.com%2Ffreepng%2Ffootball-player-silhouette_979227.html&psig=AOvVaw2KvNl3_YPkngWaitRwpeNB&ust=1529777063133854"
-campodefutebol =  "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj3rJO97ufbAhUCgZAKHbRzBGEQjRx6BAgBEAU&url=https%3A%2F%2Fpt.pngtree.com%2Ffreepng%2Fgreen-stadium-upload_3102997.html&psig=AOvVaw0KR-Wx4KXCtfI4k4yNabp0&ust=1529777235886748"
-
+jogadordefutebol = ""
+campodefutebol  = ""
 def Historia():
-	cenaCampo = Cena(img = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj3rJO97ufbAhUCgZAKHbRzBGEQjRx6BAgBEAU&url=https%3A%2F%2Fpt.pngtree.com%2Ffreepng%2Fgreen-stadium-upload_3102997.html&psig=AOvVaw0KR-Wx4KXCtfI4k4yNabp0&ust=1529777235886748")
+	cenaCampo = Cena(img = "")
 	cenaCampo.vai()
 Historia()
     
