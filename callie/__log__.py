694765102e1842040b5191c18a0dc2e433e26dd4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-25 15:13:11 -0300

    Created callie/__log__.py at Timestamp: 2018-06-25 18:13:11

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 25 2018 15:13:08.598 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  batata doce = "https://i.imgur.com/OkdQ390.jpg"
+          ^
+SyntaxError: invalid syntax
+'''},
