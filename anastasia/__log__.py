0c5a8b9c5b3c075ebd24b322ae09a0a5493bffb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-25 07:43:42 -0300

    Saved anastasia/__log__.py at Timestamp: 2018-06-25 10:43:42

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -290,3 +290,21 @@ AttributeError: 'function' object has no attribute 'norte'
     gimnasium_ = _sala = Sala(gimnasium_n,gimnasium_l,gimnasium_s,gimnasium_o, "trig")
 NameError: name 'gimnasium_o' is not defined
 '''},
+{'date': 'Mon Jun 25 2018 07:43:41.153 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    gimnasium().norte.vai()
+  module <module> line 27
+    children_ = Elemento(napkin_holder, tit = "reflector", drag=True,
+NameError: name 'napkin_holder' is not defined
+'''},
