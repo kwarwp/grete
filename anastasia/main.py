c6647887b0497df89d8d382b405f74a0dfe1ad8c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-25 14:35:06 -0300

    Saved anastasia/main.py at Timestamp: 2018-06-25 17:35:06

--- anastasia/main.py
+++ anastasia/main.py
@@ -52,7 +52,7 @@ def gimnasium():
     from naomi.main import Elemento
     _sala.sul.meio.vai = go_gimnasiun
     children_ = Elemento(children, tit = "reflector", drag=True,
-        x = 310, y = 450, w = 70, h = 60, drop="children",
+        x = 200, y = 188, w = 150, h = 200, drop="children",
         cena=_sala.norte, texto="please, help me, fix my name")
     bars_ = Elemento(bars, tit = "grandstand", drag=True,
         x = 160, y = 210, w = 80, h = 100, drop="bars",
