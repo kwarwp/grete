a8ccb8507eb7a5fa118b0dbdd50182d56377aaef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-25 09:53:33 -0300

    Saved anastasia/__log__.py at Timestamp: 2018-06-25 12:53:33

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -326,3 +326,21 @@ NameError: name 'napkin_holder' is not defined
     light_post_ = Elemento(ligth_post, tit = "children", drag=True,
 NameError: name 'ligth_post' is not defined
 '''},
+{'date': 'Mon Jun 25 2018 09:52:20.401 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    gimnasium().norte.vai()
+  module <module> line 27
+    children_ = Elemento(napkin_holder, tit = "reflector", drag=True,
+NameError: name 'napkin_holder' is not defined
+'''},
