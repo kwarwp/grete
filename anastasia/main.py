e52fd1b1b46a704c73d3214b07a8d2b771e2bfee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-25 09:56:02 -0300

    Saved anastasia/main.py at Timestamp: 2018-06-25 12:56:02

--- anastasia/main.py
+++ anastasia/main.py
@@ -24,22 +24,22 @@ def gimnasium():
     gimnasium_ = _sala = Sala(gimnasium_n,gimnasium_l,gimnasium_s,gimnasium_o, "trig")
     from naomi.main import Elemento
     _sala.sul.meio.vai = vai_geo
-    children_ = Elemento(napkin_holder, tit = "reflector", drag=True,
+    children_ = Elemento(children, tit = "reflector", drag=True,
         x = 310, y = 450, w = 70, h = 60, drop="children",
         cena=_sala.norte, texto="please, help me, fix my name")
-    bars_ = Elemento(microwave, tit = "grandstand", drag=True,
+    bars_ = Elemento(bars, tit = "grandstand", drag=True,
         x = 160, y = 210, w = 80, h = 100, drop="bars",
         cena=_sala.oeste, texto="please, help me, fix my name")
-    grandstand_ = Elemento(spray, tit = "bars",
+    grandstand_ = Elemento(grandstand, tit = "bars",
         x = 360, y = 70, w = 80, h = 70, drop="grandstand",
         cena=_sala.leste, texto="please, help me, fix my name")
-    reflector_ = Elemento(vase, tit = "ball", drag=True,
+    reflector_ = Elemento(reflector, tit = "ball", drag=True,
         x = 300, y = 275, w = 100, h = 140, drop="reflector",
         cena=_sala.norte, texto="please, help me, fix my name")
-    light_post_ = Elemento(ligth_fixture, tit = "children", drag=True,
+    light_post_ = Elemento(light_post, tit = "children", drag=True,
         x = 50, y = 40, w = 100, h = 100, drop="light_post",
         cena=_sala.sul, texto="please, help me, fix my name")
-    ball_ = Elemento(dispenser, tit = "ligthpost",
+    ball_ = Elemento(ball, tit = "ligthpost",
         x = 30, y = 500, w = 100, h = 120,drop="ball",
         cena=_sala.leste, texto="please, help me, fix my name")
     return _sala
