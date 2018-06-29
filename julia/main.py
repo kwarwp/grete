a108f3920086063bdef8d5bc0a4470bb6b2bedbd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 15:03:43 -0300

    Saved julia/main.py at Timestamp: 2018-06-29 18:03:43

--- julia/main.py
+++ julia/main.py
@@ -6,7 +6,9 @@ campodefutebol  = "https://papel.deparede.com.br/wp-content/uploads/2011/05/camp
 def Historia():
 	cenaCampo = Cena(img = "https://papel.deparede.com.br/wp-content/uploads/2011/05/campo-de-futebol-330x247.jpg")
 	cenaCampo.vai()
-	jogador_defutebol = Elemento(img = "https://png.pngtree.com/element_origin_min_pic/16/09/10/1357d39a3fa4fe0.jpg", tit = "Jogador", style = dict (top = 60, left = 150 , height = 150, width = 50))
+	jogador_defutebol = Elemento(img = "https://png.pngtree.com/element_origin_min_pic/16/09/10/1357d39a3fa4fe0.jpg", tit = "Jogador", style = dict (top = 50, left = 50 , height = 30, width = 40 ))
 	jogador_defutebol.entra(cenaCampo)
+	txtfala = Texto(cenaCampo, "Gostaria de jogar futebol aqui, se possivel.")
+	jogador_defutebol.vai = txtfala.vai
 Historia()
     
