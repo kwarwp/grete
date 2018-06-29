d5ae30421f7cee9307914c4e8789cbe4391b762b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 14:45:45 -0300

    Saved julia/main.py at Timestamp: 2018-06-29 17:45:44

--- julia/main.py
+++ julia/main.py
@@ -1,10 +1,12 @@
 # grete.julia.main.py
-from _spy.vitollino.main import STYLE, INVENTARIO, Sala, Texto, Cena
+from _spy.vitollino.main import STYLE, INVENTARIO, Sala, Texto, Cena, Elemento
 
-jogadordefutebol = ""
-campodefutebol  = ""
+jogadordefutebol = "https://png.pngtree.com/element_origin_min_pic/16/09/10/1357d39a3fa4fe0.jpg"
+campodefutebol  = "https://papel.deparede.com.br/wp-content/uploads/2011/05/campo-de-futebol-330x247.jpg"
 def Historia():
-	cenaCampo = Cena(img = "")
+	cenaCampo = Cena(img = "https://papel.deparede.com.br/wp-content/uploads/2011/05/campo-de-futebol-330x247.jpg")
 	cenaCampo.vai()
+	jogador_defutebol = Elemento(img = "https://png.pngtree.com/element_origin_min_pic/16/09/10/1357d39a3fa4fe0.jpg", tit = "Jogador", style = dict (top = 60, left = 150 , height = 150, width = 50))
+	jogador_defutebol.entra(cenaCampo)
 Historia()
     
