16b4e6bc9baf594ebb8b9c6fd836baff6902904e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 15:00:35 -0300

    Saved morgan/main.py at Timestamp: 2018-07-13 18:00:35

--- morgan/main.py
+++ morgan/main.py
@@ -1,4 +1,8 @@
 # grete.morgan.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto,STYLE,Dragger, Droppable, INVENTARIO, Sala
+STYLE["width"] = 800
+STYLE["height"] = "600px"
+TIRINHA_PRINCIPALS_ROOM = "https://i.imgur.com/UsGRBIp.jpg"
 Principals_room_n = "https://i.imgur.com/U9ixbT0.jpg"
 Principals_room_o = "https://i.imgur.com/gxOcDsS.jpg"
 Principals_room_l = "https://i.imgur.com/SV1Ko1N.jpg"
@@ -11,10 +15,7 @@ keyboard = "https://i.imgur.com/EM4mZq2.jpg"
 power_plug ="https://i.imgur.com/4eX2x0y.jpg"
 pipe = "https://i.imgur.com/IWX6ah1.jpg"
 caixa_mae = "https://i.imgur.com/THLu9uv.jpg"
-from _spy.vitollino.main import Cena,Elemento,Texto,STYLE,Dragger, Droppable, INVENTARIO, Sala
-STYLE["width"] = 800
-STYLE["height"] = "600px"
-TIRINHA_DO_CLAUDEMILSON = "https://i.imgur.com/vIYgSQV.jpg"
+
 PRR = None
 def principalsroom():
     global principalsroom_
@@ -23,33 +24,39 @@ def principalsroom():
         
     def _gone_principalsroom():
         try:
-            gymnasium().sul.vai()
+            principalsroom().sul.vai()
         except:
-            from libby.main import gymansium
+            from libby.main import gymnasium
             gymnasium().sul.vai()
-        def _go_principalsroom():
-            principalsroom_.sul.meio.vai = _gone_principalsroom
-            _vai = Cena(PRINCIPALS_ROOM)
+    def _go_principalsroom():
+        principalsroom_.sul.meio.vai = _gone_principalsroom
+        _vai = Cena(TIRINHA_PRINCIPALS_ROOM)
         def redir():
-            _vai.vai = _principalsroom
-        historia = Cena(PRINCIPALS_ROOM, _vai, _vai, _vai)
-        texto = """                        Cleison goes to the principalÃÂ´s room
+            _vai.vai = _gone_principalsroom
+        historia = Cena(TIRINHA_PRINCIPALS_ROOM, _vai, _vai, _vai)
+        texto = """Cleison goes to the principals room
 Cleison hears someone calling him. He did not know who it was. He looked and saw a woman wearing a t-shirt with the words : Become Claudemilson. 
 She said:
--Go to the PrincipalÃÂ´s room!!!!
+-Go to the Principals room!!!!
 He remembered that  in his time he had a principal named George that was cool.
-He did not understand anything, only followed the woman. In the principalÃÂ´s room, she said
+He did not understand anything, only followed the woman. In the principas room, she said
 -You will have to listen to a rap and a funk of the dictatorship of Claudemilson and eat raw noodles!!!
-Out of nowhere started a beat: Become ClaudemilsonÃ¢ÂÂ« Ã¢ÂÂª
+Out of nowhere started a beat: Become Claudemilson
 Wile Claudemilson was not seeing, Claison escaped.
 When he ran away, he got into his car and went around desperately, but a traffic policeman stopped him . In this meanwhile, Claudemilson had time to reach him.Claudemilson began to shout with Cleison and in the meantime a fiscal that passed near there decided to enter the school .He saw that Claudemilson tortured the children, and she was arrested.
 """ 
         _vai.vai = Texto(historia, '', texto, foi=redir).vai
         historia.vai()
+    def go_principalsroom():
+        _go_principalsroom()
         
-    PRR = _sala = Sala(Principals_room_n,Principals_room_o,Principals_room_l,Principals_room_s, "principal")
+    def __go_principalsroom():
+        from naomi.main import secretaria
+        secretaria().secretaria.vai()
+        
+    principalsroom_ = _sala = Sala(Principals_room_n,Principals_room_o,Principals_room_l,Principals_room_s, "principal")
     from naomi.main import Elemento
-    _Sala.sul.meio.vai = vai_principalsroom
+    _sala.sul.meio.vai = go_principalsroom
     caixa = Elemento(caixa_mae, tit = "pipe", drag=True,
         x = 480, y = 140, w = 180, h = 290, drop="glow ball",
        cena=_sala.norte, texto="please, help me, fix my name")
