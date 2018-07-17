233d8253aae9d82d8a40afc17abef5df44dad9ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-17 02:45:05 -0300

    Saved courtney/__log__.py at Timestamp: 2018-07-17 05:45:05

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -205,3 +205,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Jul 17 2018 02:45:04.672 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    school_house_().norte.vai()
+  module <module> line 51
+    _sala.sul.meio.vai = _go_school_house
+NameError: name '_go_school_house' is not defined
+'''},
