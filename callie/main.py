b3d38b8266ca0e3b572130edd1349422abafabc5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-16 23:32:03 -0300

    Saved callie/main.py at Timestamp: 2018-07-17 02:32:03

--- callie/main.py
+++ callie/main.py
@@ -2,8 +2,8 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 kit_n = "https://i.imgur.com/98JoHe0.jpg"
 kit_s = "http://i.imgur.com/0Ix7Dd3.jpg"
-kit_o = "https://i.imgur.com/0Zztrs5.jpg"
-kit_l = "https://i.imgur.com/S12NoXf.jpg"
+kit_l = "https://i.imgur.com/0Zztrs5.jpg"
+kit_o = "https://i.imgur.com/S12NoXf.jpg"
 
 #Objects:
 grao_de_bico = "https://i.imgur.com/JdP7kbV.jpg"
@@ -30,10 +30,10 @@ def kitchen():
     _sala.sul.meio.vai = vai_geo
     grao = Elemento(grao_de_bico, tit = "suction hood", drag=True,
         x = 670, y = 390, w = 100, h = 100, drop="chick peas",
-       cena=_sala.sul, texto="Please help me, fix my name.")
+       cena=_sala.oeste, texto="Please help me, fix my name.")
     coif = Elemento(coifa, tit = "chick peas", drag=True,
         x = 0, y = 0, w = 700, h = 100, drop="suction hood",
-        cena=_sala.oeste, texto="Please help me, fix my name.")
+        cena=_sala.leste, texto="Please help me, fix my name.")
 
     sweet_potato = Elemento(sweetpotato, tit = "olive oil", drop="sweet potato",
         x = 740, y = 500, w = 60, h = 60,
@@ -43,13 +43,13 @@ def kitchen():
        cena=_sala.norte, texto="Please help me, fix my name.")
     doorl = Elemento(door_lock, tit = "stove", drag=True,
         x = 440, y = 420, w = 80, h = 100, drop="door lock",
-        cena=_sala.sul, texto="Please help me, fix my name.")
+        cena=_sala.oeste, texto="Please help me, fix my name.")
     adocante = Elemento(sweetner, tit = "door lock", drop="sweetner",
         x = 350, y = 530, w = 100, h = 70,
         cena=_sala.norte, texto="Please help me, fix my name.")
     cokie = Elemento(fogao, tit = "sweetner", drop="stove",
         x = 500, y = 500, w = 100, h = 120,
-        cena=_sala.oeste, texto="Please help me, fix my name.")
+        cena=_sala.leste, texto="Please help me, fix my name.")
 
     return _sala
 
