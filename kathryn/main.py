6b2937ba6b6e3a43624e36aea049db4f1ddbe24b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-17 02:03:29 -0300

    Saved kathryn/main.py at Timestamp: 2018-07-17 05:03:28

--- kathryn/main.py
+++ kathryn/main.py
@@ -33,6 +33,7 @@ def trigonometria():
     TRIG = _sala = Sala(CLASS1,CLASS2,CLASS3,CLASS4, "trig")
     from naomi.main import Elemento
     _sala.oeste.meio = Cena(vai=_go_geography)
+    #_sala.oeste.meio.vai = _go_geography
     parap = Elemento(parapeito, tit = "socket", drag=True,
         x = 610, y = 300, w = 80, h = 90, drop="sill",
         cena=_sala.norte, texto="Please help me, fix my name.")
@@ -78,7 +79,7 @@ def aventurasnaescola():
     leon = Elemento(img = LEON, tit = "leon", style = dict(left = 350, top = 60, widht = 60, height = 200))
     leon.entra(class1)
     globos.entra(class1)
-    txtleon = Texto(class1,"Eu quero viajar para cÃÂÂÂÂÂÂÃÂÂÂÂÂÂÂÂÂÂÂÂÂ¡!!")
+    txtleon = Texto(class1,"Eu quero viajar para cÃÂÂÂÂÂÂÂÃÂÂÂÂÂÂÂÂÂÂÂÂÂÂÂ¡!!")
     globos.entra(class2)
     leon.entra(class2)
     txtglobos = Texto(class2,"Ei,cuidado!!")
