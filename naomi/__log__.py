c12cda9c5f816e23e87b814fcc9a1f8b77ba4b5a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-17 02:07:50 -0300

    Saved naomi/__log__.py at Timestamp: 2018-07-17 05:07:50

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -76,3 +76,21 @@ NameError: name 'panstyle' is not defined
     GEO.oeste.vai()
 AttributeError: 'NoneType' object has no attribute 'oeste'
 '''},
+{'date': 'Tue Jul 17 2018 02:07:44.834 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 196
+    geografia().sul.vai()
+  module <module> line 185
+    _sala.sul.meio = Cena(vai=_go_gymnasiun)
+NameError: name '_go_gymnasiun' is not defined
+'''},
