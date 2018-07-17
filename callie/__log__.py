3f49dea36d70a2aeacf7a2f0e30c05e808b75460
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-17 02:10:26 -0300

    Saved callie/__log__.py at Timestamp: 2018-07-17 05:10:26

--- callie/__log__.py
+++ callie/__log__.py
@@ -108,3 +108,21 @@ NameError: name 'Principals_room_n' is not defined
     sweet_potato = Elemento(sweetpotato, tit = "olive oil", drop="sweet potato",
 NameError: name 'sweetpotato' is not defined
 '''},
+{'date': 'Tue Jul 17 2018 02:10:25.455 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 82
+    kitchen().norte.vai()
+  module <module> line 53
+    _sala.sul.meio.vai = vai_geo
+NameError: name 'vai_geo' is not defined
+'''},
