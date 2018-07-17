c7db2fede363ff62618e9b8a943e877a48fee3bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-17 02:21:53 -0300

    Saved anastasia/main.py at Timestamp: 2018-07-17 05:21:52

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,10 +1,8 @@
 # grete.anastasia.main.py
-#REVISADO
-#COLOCAR PAREDES CORRETAS
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE,Dragger, Droppable, INVENTARIO, Sala
 STYLE["width"] = 800
 STYLE["height"] = "600px"
-TIRINHA_DO_CLAUDEMILSON = "https://i.imgur.com/zP65MT5.jpg"
+TIRINHA_DO_CLAUDEMILSON = "https://i.imgur.com/vIYgSQV.jpg"
 child = "https://i.imgur.com/SAjYpW5.jpg"
 bars = "https://i.imgur.com/4g9dJdv.jpg"
 grandstand = "https://i.imgur.com/6PYwS0s.jpg"
@@ -29,12 +27,12 @@ def gimnasium():
             secretary().sul.vai()
         
     def _go_gimnasiun():
-        gimnasium_.sul.meio.vai = _gone_gimnasiun
-        _vai = Cena(TIRINHA_DO_CLAUDEMILSON)
+        gimnasium_.sul.meio = Cena(vai=_gone_gimnasiun)
+        _vai = Cena()
         def redir():
             _vai.vai = _gone_gimnasiun
         historia = Cena(TIRINHA_DO_CLAUDEMILSON, _vai, _vai, _vai)
-        texto = """ One day, there was a manifestation against Claudemilson, and she did not like it. She made the demonstrators hear "Funk Carioca" during 10 hours... """ 
+        texto = """ One day, there was a manifestation against Claudemilson, and she didnÂ´t like it. She made the people of the manifestation hear Funk Carioca during 10 hours.. """ 
         _vai.vai = Texto(historia, '', texto, foi=redir).vai
         historia.vai()
         
@@ -61,27 +59,27 @@ def gimnasium():
         geografia().sul.vai()
     gimnasium_ = _sala = Sala(gimnasium_n,gimnasium_l,gimnasium_s,gimnasium_o, "gimnasium")
     from naomi.main import Elemento
-    _sala.sul.meio.vai = go_gimnasiun
-    _sala.oeste.meio.vai = go_school_house
-    _sala.norte.meio.vai = go_geography
+    _sala.norte.meio = Cena(vai=go_gimnasiun)
+    _sala.oeste.meio = Cena(vai=go_school_house)
+    _sala.sul.meio = Cena(vai=go_geography)
     child_ = Elemento(child, tit = "reflector", drag=True,
         x = 210, y = 160, w = 334, h = 213, drop="child",
-        cena=_sala.sul, texto="Please help me, fix my name.")
+        cena=_sala.sul, texto="please, help me, fix my name")
     bars_ = Elemento(bars, tit = "grandstand", drag=True,
         x = 375, y = 3, w = 130, h = 70, drop="bars",
-        cena=_sala.norte, texto="Please help me, fix my name.")
+        cena=_sala.norte, texto="please, help me, fix my name")
     grandstand_ = Elemento(grandstand, tit = "bars",
         x = 330, y = 188, w = 187, h = 180, drop="grandstand",
-        cena=_sala.oeste, texto="Please help me, fix my name.")
+        cena=_sala.oeste, texto="please, help me, fix my name")
     reflector_ = Elemento(reflector, tit = "ball", drag=True,
         x = 700, y = 13, w = 100, h = 140, drop="reflector",
-        cena=_sala.oeste, texto="Please help me, fix my name.")
+        cena=_sala.oeste, texto="please, help me, fix my name")
     light_post_ = Elemento(light_post, tit = "child", drag=True,
-        x = 300, y = 2, w = 400, h = 380, drop="light post",
-        cena=_sala.leste, texto="Please help me, fix my name.")
-    ball_ = Elemento(ball, tit = "light post",
+        x = 300, y = 2, w = 400, h = 380, drop="light_post",
+        cena=_sala.leste, texto="please, help me, fix my name")
+    ball_ = Elemento(ball, tit = "ligthpost",
         x = 358, y = 210, w = 40, h = 40,drop="ball",
-        cena=_sala.norte, texto="Please help me, fix my name.")
+        cena=_sala.norte, texto="please, help me, fix my name")
     return _sala
 
 
