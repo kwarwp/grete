b5e8739998cb512c5c1e47e8d777aa4ffd2a7189
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-16 22:05:52 -0300

    Saved callie/__log__.py at Timestamp: 2018-07-17 01:05:52

--- callie/__log__.py
+++ callie/__log__.py
@@ -72,3 +72,21 @@ NameError: name 'Principals_room_n' is not defined
     PRR = _sala = Sala(Principals_room_n,Principals_room_o,Principals_room_l,Principals_room_s, "principal")
 NameError: name 'Principals_room_n' is not defined
 '''},
+{'date': 'Mon Jul 16 2018 22:05:46.37 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    kitchen().norte.vai()
+  module <module> line 27
+    PRR = _sala = Sala(Principals_room_n,Principals_room_o,Principals_room_l,Principals_room_s, "principal")
+NameError: name 'Principals_room_n' is not defined
+'''},
