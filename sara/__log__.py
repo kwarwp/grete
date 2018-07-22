c2b00e6dd13c005df6f76cd4c4b341b0ffa17b6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 18:36:31 -0300

    Created sara/__log__.py at Timestamp: 2018-07-22 21:36:31

---
+++ sara/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Sun Jul 22 2018 18:36:15.348 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    dogatthebeach()
+  module <module> line 9
+    txtfunnydog = Texto(cenabeach,"do you need dolly?")
+NameError: name 'cenabeach' is not defined
+'''},
