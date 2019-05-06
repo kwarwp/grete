732f8c7b012aca8c4428f1fc342b792469b8abf5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-06 10:58:12 -0300

    Created kristen/__log__.py at Timestamp: 2019-05-06 13:58:12

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon May 06 2019 10:58:08.914 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  HARRY POTTER= "http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bd6b.png "
+         ^
+SyntaxError: invalid syntax
+'''},
