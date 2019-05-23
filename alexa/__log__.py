31f2ceda4e63a919f2908107caa2ab4d72950e20
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:16:48 -0300

    Saved alexa/__log__.py at Timestamp: 2019-05-23 13:16:48

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -38,3 +38,19 @@ TypeError: 'module' object is not callable
     paque = Cena(CENA_PARQUE)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu May 23 2019 10:16:56.329 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    parque = Cena(CENA_PARQUE)
+TypeError: 'module' object is not callable
+'''},
