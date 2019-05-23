2654081bdd41dc6b0d20cd2911c074c58e727fd8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:15:43 -0300

    Created alexa/__log__.py at Timestamp: 2019-05-23 13:15:43

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu May 23 2019 10:15:50.54 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  CENA_PARQUE = https://i.imgur.com/kqWvogN.jpg
+                      ^
+SyntaxError: invalid syntax
+'''},
