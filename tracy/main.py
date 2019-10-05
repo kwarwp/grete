658a4f8b5d7cef3968287219129d7c944068b02d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-05 11:16:49 -0300

    Saved tracy/main.py @2019-10-05 14:16

--- tracy/main.py
+++ tracy/main.py
@@ -1,16 +1,1 @@
 # grete.tracy.main.py
-from _spy.vitollino.main import Cena, Elemento, Texto
-CASTELO="https://http2.mlstatic.com/painel-lona-fosca-castelo-cinderela-2-x-150envio-48hs-D_NQ_NP_405405-MLB25019727073_082016-F.jpg"
-PRINCESA="https://i.pinimg.com/236x/c9/72/7f/c9727fb053357c5f764297d170d54231--disney-princess-cupcakes-disney-princess-ariel.jpg" 
-CASAL="https://1.bp.blogspot.com/-Kpu0Abwjqe0/TyS1cAygHGI/AAAAAAAADhk/eWsZn-I6vXs/s1600/Ariel-and-Eric-disney-couples-500-500.jpg" 
-UNDER_THE_SEA=""
-def historia_com_castelo():
-    cena_com_castelo = Cena(img=CASTELO)
-    princesa = Elemento(img=PRINCESA, tit="bom dia a todos meu nome Ã© Ariel ", style=dict(width=100, height=150))
-    princesa.entra(cena_com_castelo)
-    fala_ariel = Texto(cena_com_castelo,"pode nÃ£o parecer mas eu guardo vÃ¡rios segredos como que eu sou uma sereia!")
-    princesa.vai = fala_ariel.vai
-    cena_com_castelo.vai()
-    
-    
-historia_com_castelo()
