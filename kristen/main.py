a304cbb286cde4e098b3ae1581c2fc731919bc0c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-05 11:15:06 -0300

    Saved kristen/main.py @2019-10-05 14:15

--- kristen/main.py
+++ kristen/main.py
@@ -1,11 +1,3 @@
 # grete.kristen.main.py
-from _spy.vitollino.main import Cena,Texto,Elemento
-from _spy.vitollino.main import Invetario as INV
-HARRY POTTER= "http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bd6b.png "
-BECO DIAGONAL= " http://portal.julund.com.br/wp-content/uploads/2013/07/beco-diagonal.jpg"
-VOLDEMORT= "https://banner2.kisspng.com/20180203/lqw/kisspng-lord-voldemort-harry-potter-and-the-philosophers-s-harry-potter-png-photo-5a75e014c020a1.773443811517674516787.jpg"
-SALA COMUNAL DA SONSERINA= "https://images.pottermore.com/bxd3o8b291gf/1hdHe90ZTMcAM0g4YO0YgI/4c825d8b83d2f9ed41dfad282ae63f19/SlytherinCommonRoom_PM_B2C12M3_SlytherinCommonRoomWithDracoMalfoy_Moment.jpg"
 
-def hogwarts ():
-    salao principal= Cena ( img= SALAO PRINCIPAL)
     
