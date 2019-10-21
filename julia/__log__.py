4acaddb933429c5e98741117c24c5a9633bb5554
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-21 11:52:55 -0300

    Saved julia/__log__.py @2019-10-21 14:52

--- julia/__log__.py
+++ julia/__log__.py
@@ -63,3 +63,10 @@ TypeError: 'module' object is not callable
     ESCOLA
 NameError: name 'ESCOLA' is not defined
 '''},
+{'date': 'Mon Oct 21 2019 11:53:30.806 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  class Jogo
+             ^
+SyntaxError: invalid syntax
+'''},
