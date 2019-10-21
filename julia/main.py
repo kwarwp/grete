c2f2229399537dccecb29fbfad8b7773496ae00b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-21 12:00:20 -0300

    Saved julia/main.py @2019-10-21 15:00

--- julia/main.py
+++ julia/main.py
@@ -1,3 +1,13 @@
-# grete.julia.main.py
-
-    
+# grete.julia.main.py    
+from _spy.vitollino.main import Cena
+QUARTO="https://i.imgur.com/awgqLtb.jpg"
+ESCOLA="https://i.imgur.com/DtEkan7.jpg"
+class Jogo:
+	def inicia (self):
+         primeira_Cena=Cena(QUARTO)
+         segunda_Cena=Cena(ESCOLA)
+         primeira_Cena.direita=segunda_Cena
+         primeira_Cena.vai()
+jogo=Jogo()
+jogo.inicia()
+        
