726f776bac7c9e7167835e17fea50f8523c0f6e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:26:57 -0300

    Saved adda/main.py @2019-11-01 14:26

--- adda/main.py
+++ adda/main.py
@@ -1,4 +1,11 @@
 # grete.adda.main.py
-from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import Cena ,Elemento
 from _spy.vitollino.main import INVENTARIO as inv 
-FLORESTA="
+FLORESTA="https://static.wixstatic.com/media/915324_a7aefd445f9c4b9d8703509a351c0258.jpg/v1/fill/w_637,h_311,al_c,q_80,usm_0.66_1.00_0.01/915324_a7aefd445f9c4b9d8703509a351c0258.webp"
+ELEMENTO="http://www.dlf.pt/png/big/1/11494_soldado-png.png"
+class wlg():
+    floresta= Cena(img=FLORESTA)
+    elemento= Elemento(img=ELEMENTO)
+    elemento.entra(floresta)
+    floresta.vai()
+wlg()
