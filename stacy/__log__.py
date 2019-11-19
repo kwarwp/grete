105366b78df4d0752df5abca79f4710b98df2c62
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 14:30:58 -0300

    Saved stacy/__log__.py @2019-11-19 17:30

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -201,3 +201,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 19 2019 14:30:55.546 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  INVENTARIO.inicia()
+  ^
+IndentationError: unexpected indent
+'''},
