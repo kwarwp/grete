62232ba3a04c164d0d7ac8e5451d7dcb9b8a8b36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 14:30:33 -0300

    Created roxanne/__log__.py @2019-11-19 17:30

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 19 2019 14:30:29.45 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaParque = Cena(img = "http://www.fundospaisagens.com/Imagens/parque-com-arvores.jpg"
+                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
