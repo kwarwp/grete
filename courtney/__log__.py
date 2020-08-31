5544595256b417ac327ca751425cf4f99781caac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-31 18:00:12 -0300

    Saved courtney/__log__.py @2020-08-31 21:00

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -223,3 +223,25 @@ IndentationError: unexpected indent
     _sala.sul.meio.vai = _go_school_house
 NameError: name '_go_school_house' is not defined
 '''},
+{'date': 'Mon Aug 31 2020 18:00:11.604 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    school_house_().norte.vai()
+  module <module> line 53
+    napkin_holder_ = Elemento(napkin_holder, tit = "vase", drag=True,
+  module naomi.main line 27
+    self.img, self.title, self.real, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 651
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
