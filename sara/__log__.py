14ea0bcf646f545380e7bb51cf75ce4ea19b388d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-24 23:55:25 -0300

    Saved sara/__log__.py @2022-05-25 02:55

--- sara/__log__.py
+++ sara/__log__.py
@@ -35,3 +35,21 @@ NameError: name 'cenabeach' is not defined
     txtfunnydog = Texto(cenabeach,"do you need dolly?")
 NameError: name 'cenabeach' is not defined
 '''},
+{'date': 'Tue May 24 2022 23:55:25.189 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    dogatthebeach()
+  module <module> line 9
+    txtfunnydog = Texto(cenabeach,"do you need dolly?")
+NameError: name 'cenabeach' is not defined
+'''},
