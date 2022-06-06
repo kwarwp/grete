5916b612c1162042334ee5328c9ff1e93bd4ea0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 11:38:34 -0300

    Start Agora

--- __author__.py
+++ __author__.py
@@ -1,2 +1,2 @@
 {
-"grete": {"author_nick": "dfssf", "author_name": "sdfd", "author_email": "akakaka@gmail.com", "author_org": "", "author_site": "", "author_public": "on"},
+"grete": {"author_nick": "ST_Agora_18", "author_name": "ST_Agora_18", "author_email": "akakaka@gmail.com", "author_org": "Agora", "author_site": "http://colegioagoraniteroi.com.br/", "author_public": "on"},
