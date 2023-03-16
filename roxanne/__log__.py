43a71f87e74f9e8b6ece64258abd46375702116b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-03-16 18:31:49 -0300

    Saved roxanne/__log__.py @2023-03-16 21:31

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -6,3 +6,10 @@
                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Mar 16 2023 18:31:49.196 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaParque = Cena(img = "http://www.fundospaisagens.com/Imagens/parque-com-arvores.jpg"
+                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
