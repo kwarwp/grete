6033e4ced2c5c129586c3e770b93e274d1cd5ba2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-04-05 18:50:13 -0300

    Saved naomi/__log__.py @2024-04-05 21:50

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -122,3 +122,25 @@ Exception: <RangeError: Maximum call stack size exceeded>
 'error': '''
 Exception: <RangeError: Maximum call stack size exceeded>
 '''},
+{'date': 'Fri Apr 05 2024 18:50:12.760 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 213
+    geografia().sul.vai()
+  module <module> line 192
+    mic = Elemento(MIC, tit="volcano", drag=False, drop="microscope",
+  module <module> line 27
+    self.img, self.title, self.real, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
