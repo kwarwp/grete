b70fae75a8298ea396126ec7dd0c2f4684477551
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-04-05 18:46:38 -0300

    Saved callie/__log__.py @2024-04-05 21:46

--- callie/__log__.py
+++ callie/__log__.py
@@ -126,3 +126,25 @@ NameError: name 'sweetpotato' is not defined
     _sala.sul.meio.vai = vai_geo
 NameError: name 'vai_geo' is not defined
 '''},
+{'date': 'Fri Apr 05 2024 18:46:38.144 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    kitchen().norte.vai()
+  module <module> line 54
+    grao = Elemento(grao_de_bico, tit = "suction hood", drag=True,
+  module naomi.main line 27
+    self.img, self.title, self.real, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
