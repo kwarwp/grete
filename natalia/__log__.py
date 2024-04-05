2cf9802a8303e4960179340e4886938876904e88
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-04-05 18:45:46 -0300

    Created natalia/__log__.py @2024-04-05 21:45

---
+++ natalia/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Fri Apr 05 2024 18:45:45.34 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    pleasereadthis()
+  module <module> line 9
+    txtferarri = Texto(cenastore,"I want to go for the street")
+NameError: name 'cenastore' is not defined
+'''},
