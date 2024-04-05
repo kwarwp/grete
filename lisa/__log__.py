38c49680312a6822f36219ec6247c7b659c39467
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-04-05 18:47:00 -0300

    Saved lisa/__log__.py @2024-04-05 21:47

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -65,3 +65,25 @@ NameError: name 'trigonometria' is not defined
     if ROOM:
 NameError: name 'ROOM' is not defined
 '''},
+{'date': 'Fri Apr 05 2024 18:46:59.914 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 99
+    room23().norte.vai()
+  module <module> line 78
+    ampulheta = Elemento(ampulheta_, tit = "sprayer", drag=True,
+  module naomi.main line 27
+    self.img, self.title, self.real, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
