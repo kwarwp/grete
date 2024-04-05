3d47f89ddf3c86f0bf73ae663414cf1b785b0c2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-04-05 18:42:04 -0300

    Saved libby/__log__.py @2024-04-05 21:42

--- libby/__log__.py
+++ libby/__log__.py
@@ -118,3 +118,25 @@ SyntaxError: EOL while scanning string literal
     _sala.oeste.meio = Cena(TIRINHA_DO_CLAUDEMILSON, vai = go_secretary)
 NameError: name 'TIRINHA_DO_CLAUDEMILSON' is not defined
 '''},
+{'date': 'Fri Apr 05 2024 18:42:04.216 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 108
+    secretary().norte.vai()
+  module <module> line 86
+    bebedouro_ = Elemento(bebedouro, tit = "switch", drag=True,
+  module naomi.main line 27
+    self.img, self.title, self.real, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
