70f74e37042ebefdd00765840bb6f59dbd2ef90c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-11-11 13:16:55 -0300

    Created samantha/__log__.py @2024-11-11 16:16

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2024 13:16:54.90 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 1
+  from _spy.vitollino.main  import Cena, 
+                                        ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
